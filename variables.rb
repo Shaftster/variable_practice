5c2d0036ca9b5a1425682cbeb2d0fd9658ee229d
Jermaine Wates <jwates7@protonmail.com>
2023-02-05 16:20:42 -0700

    add more lines

--- variables.rb
+++ variables.rb
@@ -2,12 +2,16 @@ name = 'biscuits'
 color = 'gray'
 size = 'small'
 
+biscuits_hiss = true
+biscuits_bark = false
+bicuits_weight = 6.5
+
+
 name_2 = 'Odie'
 color_2 = 'buff'
 size = 'medium'
-
-biscuits_hiss = true
-biscuits_bark = false
-
-bicuits_weight = 6.5
 odies_weight = 20.2
+odie_hiss = false
+odie_bark = true
+
+
