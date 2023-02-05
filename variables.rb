70be821f15a1f1775483c4fe246b88f0930e0443
Jermaine Wates <jwates7@protonmail.com>
2023-02-05 16:29:21 -0700

    add lines

--- variables.rb
+++ variables.rb
@@ -3,7 +3,7 @@ color = 'gray'
 size = 'small'
 
 biscuits_hiss = true
-biscuits_bark = false
+
 bicuits_weight = 6.5
 biscuits_age = 6
 
@@ -13,7 +13,8 @@ color_2 = 'buff'
 size = 'medium'
 odies_weight = 20.2
 odie_hiss = false
-odie_bark = true
+
 odies_age = 14
+
 
 
